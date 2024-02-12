9fd1d0db7e333d06528404e9ebc589cd4a1ad34d
Hudesde <93896068+Hudesde@users.noreply.github.com>
2024-02-12 08:07:17 -0600

    Update environment.ts

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    API_URI: "https://proyectoserver.onrender.com",
+    API_URI: "https://proyectoserver.onrender.com//api",
     API_URI_CORREO :"http://www.festersanmartin.com:3001",
     API_URI_IMAGENES :"http://www.festersanmartin.com:3001"
 
